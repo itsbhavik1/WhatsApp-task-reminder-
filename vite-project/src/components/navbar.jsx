cdacb8223fe4c4942f05e27c44f777b8f3af762e
pranavb1234 <pranavtech@rediffmail.com>
2024-06-03 17:09:54 +0530

    new UI changes

--- vite-project/src/components/navbar.jsx
+++ vite-project/src/components/navbar.jsx
@@ -1,18 +1,39 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="nav flex justify-between bg-green-200 m-3 py-4" >
-        <div className="logo">
-            <span className='font-bold text-xl mx-7'>Whatsapp Notification sender</span>
-        </div>
-        <ul className='flex gap-7 mx-8'>
-        <li className='cursor-pointer my-px hover:font-bold transition-all'>Home</li>
-        <li className='cursor-pointer my-px hover:font-bold transition-all'>Bhavik</li>
-        </ul>
-    </div>
-  )
-}
+    <header className="flex h-16 w-full items-center justify-between px-4 md:px-6">
+      <a className="flex items-center gap-2" href="#">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="h-6 w-6"
+        >
+          <path d="m8 3 4 8 5-5 5 15H2L8 3z"></path>
+        </svg>
+        <span className="text-lg font-semibold">Whatsapp Notification Sender</span>
+      </a>
+      <nav className="hidden items-center gap-6 text-sm font-medium md:flex">
+        <a className="hover:underline text-lg " href="#">
+          Home
+        </a>
+        <a className="hover:underline text-lg" href="#">
+          About
+        </a>
+        
+      </nav>
+      <button class="bg-black text-white p-3 rounded-md">
+        Get Started
+      </button>
+    </header>
+  );
+};
 
-export default Navbar
-  
+export default Navbar;
